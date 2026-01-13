027689171b8087f95212760665b22bd65955e296
Grin Nare <jaebum.yoo@gmail.com>
2026-01-13 00:05:13 -0600

    Added Comments To The Login Page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { LoginStore } from './login.store';
 
@@ -12,6 +13,7 @@ import { LoginStore } from './login.store';
 })
 export class LoginComponent {
     private store = inject(LoginStore);
+    private snackBar = inject(MatSnackBar);
 
     public isLoading = this.store.isLoading;
     public errorMessage = this.store.errorMessage;
@@ -23,6 +25,8 @@ export class LoginComponent {
             username: new FormControl('public_user', Validators.required),
             password: new FormControl('public_pass', Validators.required)
         });
+
+        this.snackBar.open("This is a demo version of Jay’s personal ledger application. It uses hard - coded sample data for demonstration purposes. The production version is backed by Amazon RDS.");
     }
 
     public login() {
